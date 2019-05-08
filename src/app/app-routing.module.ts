03f2973e82aea0664de7f679c0f6a2888c7f9965
Lucky Meng (孟潜)-云服务集团 <mengqian01@inspur.com>
2019-05-08 17:03:52 +0800

    学习完3-5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { StockComponent } from './stock/stock.component';
 import { Code404Component } from './code404/code404.component';
 import { Stock2Component } from './stock2/stock2.component';
+import { BuyerListComponent } from './buyer-list/buyer-list.component';
+import { SellerListComponent } from './seller-list/seller-list.component';
 
+// 注意：路由和组件是没有直接关系的，就是一个组件可以放在主路由里，同时也可以放在某个主路由里作为子路由
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'stock', component: StockComponent, data: [{ isPro: true }] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // 重定向路由
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'stock',
+    component: StockComponent,
+    data: [{ isPro: true }],
+    children: [
+      { path: '', component: BuyerListComponent },
+      { path: 'seller/:id', component: SellerListComponent }
+    ]
+  },
   { path: 'stock2/:id', component: Stock2Component },
   { path: '**', component: Code404Component }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { StockComponent } from './stock/stock.component';
 import { Code404Component } from './code404/code404.component';
 import { Stock2Component } from './stock2/stock2.component';
+import { BuyerListComponent } from './buyer-list/buyer-list.component';
+import { SellerListComponent } from './seller-list/seller-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { Stock2Component } from './stock2/stock2.component';
     HomeComponent,
     StockComponent,
     Code404Component,
-    Stock2Component
+    Stock2Component,
+    BuyerListComponent,
+    SellerListComponent
   ],
   imports: [
     BrowserModule,
